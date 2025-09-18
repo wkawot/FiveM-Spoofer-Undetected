308744313664ecb1b6a2bbe3b45c28b1acf30f6d
rutpaeotni <apgsssyl@outlook.com>
2025-09-18 18:58:45 +0400

    Form2.Designer.cs

---
+++ FiveM Spoofer/Form2.Designer.cs
@@ -0,0 +1,368 @@
+﻿namespace FiveM_Spoofer
+{//chres0
+    partial class Spoofer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Spoofer));
+            guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(components);
+            guna2DragControl1 = new Guna.UI2.WinForms.Guna2DragControl(components);
+            guna2ShadowForm1 = new Guna.UI2.WinForms.Guna2ShadowForm(components);
+            label1 = new Label();
+            guna2ShadowPanel1 = new Guna.UI2.WinForms.Guna2ShadowPanel();
+            guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            guna2ShadowPanel3 = new Guna.UI2.WinForms.Guna2ShadowPanel();
+            guna2Button4 = new Guna.UI2.WinForms.Guna2Button();
+            label3 = new Label();
+            guna2ShadowPanel4 = new Guna.UI2.WinForms.Guna2ShadowPanel();
+            guna2Button5 = new Guna.UI2.WinForms.Guna2Button();
+            label4 = new Label();
+            guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2ShadowPanel2 = new Guna.UI2.WinForms.Guna2ShadowPanel();
+            guna2Button3 = new Guna.UI2.WinForms.Guna2Button();
+            label2 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            guna2ShadowPanel1.SuspendLayout();
+            guna2ShadowPanel3.SuspendLayout();
+            guna2ShadowPanel4.SuspendLayout();
+            guna2ShadowPanel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // guna2Button2
+            // 
+            guna2Button2.AutoRoundedCorners = true;
+            guna2Button2.BorderColor = Color.FromArgb(192, 0, 0);
+            guna2Button2.BorderRadius = 13;
+            guna2Button2.BorderThickness = 1;
+            guna2Button2.CustomizableEdges = customizableEdges1;
+            guna2Button2.DisabledState.BorderColor = Color.DarkGray;
+            guna2Button2.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2Button2.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2Button2.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2Button2.FillColor = Color.FromArgb(16, 16, 16);
+            guna2Button2.Font = new Font("Segoe UI", 9F);
+            guna2Button2.ForeColor = Color.FromArgb(192, 0, 0);
+            guna2Button2.Location = new Point(418, 12);
+            guna2Button2.Name = "guna2Button2";
+            guna2Button2.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            guna2Button2.Size = new Size(30, 28);
+            guna2Button2.TabIndex = 3;
+            guna2Button2.Text = "X";
+            guna2Button2.Click += guna2Button2_Click;
+            // 
+            // guna2Elipse1
+            // 
+            guna2Elipse1.BorderRadius = 10;
+            guna2Elipse1.TargetControl = this;
+            // 
+            // guna2DragControl1
+            // 
+            guna2DragControl1.DockIndicatorTransparencyValue = 0.6D;
+            guna2DragControl1.TargetControl = this;
+            guna2DragControl1.UseTransparentDrag = true;
+            // 
+            // guna2ShadowForm1
+            // 
+            guna2ShadowForm1.ShadowColor = Color.FromArgb(73, 54, 140);
+            guna2ShadowForm1.TargetForm = this;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = Color.FromArgb(0, 192, 0);
+            label1.Location = new Point(12, 22);
+            label1.Name = "label1";
+            label1.Size = new Size(123, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Product : FiveM Clean";
+            label1.Click += label1_Click;
+            // 
+            // guna2ShadowPanel1
+            // 
+            guna2ShadowPanel1.BackColor = Color.Transparent;
+            guna2ShadowPanel1.Controls.Add(guna2Button1);
+            guna2ShadowPanel1.Controls.Add(label1);
+            guna2ShadowPanel1.FillColor = Color.FromArgb(16, 16, 16);
+            guna2ShadowPanel1.Location = new Point(15, 253);
+            guna2ShadowPanel1.Name = "guna2ShadowPanel1";
+            guna2ShadowPanel1.ShadowColor = Color.FromArgb(0, 192, 0);
+            guna2ShadowPanel1.Size = new Size(427, 59);
+            guna2ShadowPanel1.TabIndex = 5;
+            guna2ShadowPanel1.Paint += guna2ShadowPanel1_Paint;
+            // 
+            // guna2Button1
+            // 
+            guna2Button1.AutoRoundedCorners = true;
+            guna2Button1.BorderColor = Color.FromArgb(0, 192, 0);
+            guna2Button1.BorderRadius = 11;
+            guna2Button1.BorderThickness = 1;
+            guna2Button1.CustomizableEdges = customizableEdges9;
+            guna2Button1.DisabledState.BorderColor = Color.DarkGray;
+            guna2Button1.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2Button1.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2Button1.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2Button1.FillColor = Color.FromArgb(16, 16, 16);
+            guna2Button1.Font = new Font("Segoe UI", 9F);
+            guna2Button1.ForeColor = Color.FromArgb(0, 192, 0);
+            guna2Button1.Location = new Point(337, 16);
+            guna2Button1.Name = "guna2Button1";
+            guna2Button1.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            guna2Button1.Size = new Size(80, 24);
+            guna2Button1.TabIndex = 6;
+            guna2Button1.Text = "Clear";
+            guna2Button1.Click += guna2Button1_Click;
+            // 
+            // guna2ShadowPanel3
+            // 
+            guna2ShadowPanel3.BackColor = Color.Transparent;
+            guna2ShadowPanel3.Controls.Add(guna2Button4);
+            guna2ShadowPanel3.Controls.Add(label3);
+            guna2ShadowPanel3.FillColor = Color.FromArgb(16, 16, 16);
+            guna2ShadowPanel3.Location = new Point(15, 335);
+            guna2ShadowPanel3.Name = "guna2ShadowPanel3";
+            guna2ShadowPanel3.ShadowColor = Color.FromArgb(0, 192, 0);
+            guna2ShadowPanel3.Size = new Size(427, 59);
+            guna2ShadowPanel3.TabIndex = 7;
+            // 
+            // guna2Button4
+            // 
+            guna2Button4.AutoRoundedCorners = true;
+            guna2Button4.BorderColor = Color.FromArgb(0, 192, 0);
+            guna2Button4.BorderRadius = 11;
+            guna2Button4.BorderThickness = 1;
+            guna2Button4.CustomizableEdges = customizableEdges7;
+            guna2Button4.DisabledState.BorderColor = Color.DarkGray;
+            guna2Button4.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2Button4.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2Button4.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2Button4.FillColor = Color.FromArgb(16, 16, 16);
+            guna2Button4.Font = new Font("Segoe UI", 9F);
+            guna2Button4.ForeColor = Color.FromArgb(0, 192, 0);
+            guna2Button4.Location = new Point(337, 16);
+            guna2Button4.Name = "guna2Button4";
+            guna2Button4.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            guna2Button4.Size = new Size(80, 24);
+            guna2Button4.TabIndex = 6;
+            guna2Button4.Text = "Spoof";
+            guna2Button4.Click += guna2Button4_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.ForeColor = Color.FromArgb(0, 192, 0);
+            label3.Location = new Point(12, 22);
+            label3.Name = "label3";
+            label3.Size = new Size(124, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Product : FiveM Spoof\r\n";
+            // 
+            // guna2ShadowPanel4
+            // 
+            guna2ShadowPanel4.BackColor = Color.Transparent;
+            guna2ShadowPanel4.Controls.Add(guna2Button5);
+            guna2ShadowPanel4.Controls.Add(label4);
+            guna2ShadowPanel4.FillColor = Color.FromArgb(16, 16, 16);
+            guna2ShadowPanel4.Location = new Point(15, 87);
+            guna2ShadowPanel4.Name = "guna2ShadowPanel4";
+            guna2ShadowPanel4.ShadowColor = Color.FromArgb(0, 192, 0);
+            guna2ShadowPanel4.Size = new Size(427, 59);
+            guna2ShadowPanel4.TabIndex = 8;
+            // 
+            // guna2Button5
+            // 
+            guna2Button5.AutoRoundedCorners = true;
+            guna2Button5.BorderColor = Color.FromArgb(0, 192, 0);
+            guna2Button5.BorderRadius = 11;
+            guna2Button5.BorderThickness = 1;
+            guna2Button5.CustomizableEdges = customizableEdges5;
+            guna2Button5.DisabledState.BorderColor = Color.DarkGray;
+            guna2Button5.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2Button5.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2Button5.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2Button5.FillColor = Color.FromArgb(16, 16, 16);
+            guna2Button5.Font = new Font("Segoe UI", 9F);
+            guna2Button5.ForeColor = Color.FromArgb(0, 192, 0);
+            guna2Button5.Location = new Point(337, 18);
+            guna2Button5.Name = "guna2Button5";
+            guna2Button5.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            guna2Button5.Size = new Size(80, 24);
+            guna2Button5.TabIndex = 6;
+            guna2Button5.Text = "Telegram";
+            guna2Button5.Click += guna2Button5_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.ForeColor = Color.FromArgb(0, 192, 0);
+            label4.Location = new Point(12, 22);
+            label4.Name = "label4";
+            label4.Size = new Size(49, 15);
+            label4.TabIndex = 4;
+            label4.Text = "Contact";
+            // 
+            // guna2HtmlLabel1
+            // 
+            guna2HtmlLabel1.BackColor = Color.Transparent;
+            guna2HtmlLabel1.Location = new Point(27, 23);
+            guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            guna2HtmlLabel1.Size = new Size(3, 2);
+            guna2HtmlLabel1.TabIndex = 9;
+            guna2HtmlLabel1.Text = null;
+            // 
+            // guna2ShadowPanel2
+            // 
+            guna2ShadowPanel2.BackColor = Color.Transparent;
+            guna2ShadowPanel2.Controls.Add(guna2Button3);
+            guna2ShadowPanel2.Controls.Add(label2);
+            guna2ShadowPanel2.FillColor = Color.FromArgb(16, 16, 16);
+            guna2ShadowPanel2.Location = new Point(15, 170);
+            guna2ShadowPanel2.Name = "guna2ShadowPanel2";
+            guna2ShadowPanel2.ShadowColor = Color.FromArgb(0, 192, 0);
+            guna2ShadowPanel2.Size = new Size(427, 59);
+            guna2ShadowPanel2.TabIndex = 10;
+            // 
+            // guna2Button3
+            // 
+            guna2Button3.AutoRoundedCorners = true;
+            guna2Button3.BorderColor = Color.FromArgb(0, 192, 0);
+            guna2Button3.BorderRadius = 11;
+            guna2Button3.BorderThickness = 1;
+            guna2Button3.CustomizableEdges = customizableEdges3;
+            guna2Button3.DisabledState.BorderColor = Color.DarkGray;
+            guna2Button3.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2Button3.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2Button3.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2Button3.FillColor = Color.FromArgb(16, 16, 16);
+            guna2Button3.Font = new Font("Segoe UI", 9F);
+            guna2Button3.ForeColor = Color.FromArgb(0, 192, 0);
+            guna2Button3.Location = new Point(337, 17);
+            guna2Button3.Name = "guna2Button3";
+            guna2Button3.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            guna2Button3.Size = new Size(80, 24);
+            guna2Button3.TabIndex = 6;
+            guna2Button3.Text = "Optimize";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = Color.FromArgb(0, 192, 0);
+            label2.Location = new Point(12, 22);
+            label2.Name = "label2";
+            label2.Size = new Size(141, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Product : FiveM Optimize";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label5.ForeColor = Color.FromArgb(255, 128, 0);
+            label5.Location = new Point(19, 23);
+            label5.Name = "label5";
+            label5.Size = new Size(82, 32);
+            label5.TabIndex = 11;
+            label5.Text = "FiveM";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label6.ForeColor = Color.FromArgb(0, 192, 0);
+            label6.Location = new Point(94, 23);
+            label6.Name = "label6";
+            label6.Size = new Size(57, 32);
+            label6.TabIndex = 12;
+            label6.Text = "AIO";
+            // 
+            // Spoofer
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(16, 16, 16);
+            ClientSize = new Size(460, 411);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(guna2ShadowPanel2);
+            Controls.Add(guna2HtmlLabel1);
+            Controls.Add(guna2ShadowPanel4);
+            Controls.Add(guna2ShadowPanel3);
+            Controls.Add(guna2Button2);
+            Controls.Add(guna2ShadowPanel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Spoofer";
+            Text = "Spoofer";
+            Load += Form2_Load;
+            guna2ShadowPanel1.ResumeLayout(false);
+            guna2ShadowPanel1.PerformLayout();
+            guna2ShadowPanel3.ResumeLayout(false);
+            guna2ShadowPanel3.PerformLayout();
+            guna2ShadowPanel4.ResumeLayout(false);
+            guna2ShadowPanel4.PerformLayout();
+            guna2ShadowPanel2.ResumeLayout(false);
+            guna2ShadowPanel2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private Guna.UI2.WinForms.Guna2DragControl guna2DragControl1;
+        private Guna.UI2.WinForms.Guna2ShadowForm guna2ShadowForm1;
+        private Guna.UI2.WinForms.Guna2ShadowPanel guna2ShadowPanel1;
+        private Label label1;
+        private Guna.UI2.WinForms.Guna2ShadowPanel guna2ShadowPanel3;
+        private Guna.UI2.WinForms.Guna2Button guna2Button4;
+        private Label label3;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2ShadowPanel guna2ShadowPanel4;
+        private Guna.UI2.WinForms.Guna2Button guna2Button5;
+        private Label label4;
+        private PictureBox pictureBox1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Guna.UI2.WinForms.Guna2ShadowPanel guna2ShadowPanel2;
+        private Guna.UI2.WinForms.Guna2Button guna2Button3;
+        private Label label2;
+        private Label label5;
+        private Label label6;
+    }
+}
